e1b255e86bd38addfc9a7374fa3a4ca75e87abc2
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2024-09-21 16:31:55 -0400

    Create string.jsx

---
+++ string.jsx
@@ -0,0 +1,14 @@
+function reverseString(s)  {
+    let left = 0;
+    let right = s.length-1;
+    
+    while (left < right ) {
+
+        [s[left],s[right]= s[right],s[left]];
+        
+        //move the pointer closer to eachother
+        
+        left++;
+        right--;
+    }
+};
